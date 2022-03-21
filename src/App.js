ffc84609a170890fb47e5b5791220008ef088841
Pablo Araujo <pabloj2001@gmail.com>
2022-03-21 17:10:37 -0400

    Added nav bar and some components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './shared.css';
+import MainPage from './components/main/mainPage';
+import { NavBar } from './components/navBar';
 
-function App() {
-  return (
-    <div >
-      
-    </div>
-  );
+class App extends React.Component {
+	constructor(props){
+		super(props);
+		this.state = { currentPage: 0 };
+	}
+
+	onNavChange(page){
+		this.setState({currentPage: page});
+	}
+
+	render(){
+		return (
+			<div className='app'>
+				<NavBar onNavChange={this.onNavChange.bind(this)}/>
+				{
+					this.state.currentPage === 0 && <MainPage/>
+				}
+			</div>
+		);
+	}
 }
 
 export default App;
---
+++ src/components/main/searchBar.js
@@ -0,0 +1,7 @@
+export function SearchBar(){
+    return (
+        <div className="search-bar">
+
+        </div>
+    );
+}
